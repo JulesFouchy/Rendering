c22461ff4f2d9164cbe739b2baeb7286559460b0
Jules Fouchy <jules.fouchy@ntymail.com>
2024-03-27 20:08:17 +0100

    Fix broken link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -94,15 +94,15 @@ const config: Config = {
     footer: {
       style: "dark",
       links: [
-        {
-          title: "Docs",
-          items: [
-            {
-              label: "Tutorial",
-              to: "/docs/intro",
-            },
-          ],
-        },
+        // {
+        //   title: "Docs",
+        //   items: [
+        //     {
+        //       label: "Tutorial",
+        //       to: "/docs/intro",
+        //     },
+        //   ],
+        // },
         {
           title: "Community",
           items: [
